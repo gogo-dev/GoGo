cc620a73967773b1a96529ed580d7d26fca16fee
Jacob <jacob@trinityent.org>
2010-05-27 01:02:13 +0000

    Added checksum.

--- gogo/src/PacketCrypt.cpp
+++ gogo/src/PacketCrypt.cpp
@@ -41,4 +41,20 @@ namespace packet
 
 		return packet - index;
 	}
+
+	uint16_t checksum(uint8_t* packet, int length, int index)
+	{
+		uint32_t intermediateValues[4] = {0};
+
+		for (int i = 0; i < 4; ++i)
+			intermediateValues[0] += packet[index + i];
+		
+		for (int i = 6; i < length; ++i)
+			intermediateValues[1] += intermediateValues[index + i];
+
+		intermediateValues[2] = intermediateValues[1] - intermediateValues[0];
+		intermediateValues[3] = intermediateValues[2] >> 0x10;
+
+		return (uint16_t)(intermediateValues[2] + intermediateValues[3]);
+	}
 }
--- gogo/src/PacketCrypt.h
+++ gogo/src/PacketCrypt.h
@@ -14,4 +14,8 @@ namespace packet
 	// and the buffer "packet" is decrypted.
 	boost::uint8_t* encrypt(boost::uint8_t* packet, int length, int index,
 			const boost::uint8_t* key);
+
+	// Takes a packet, the packet length, the index to start checksumming
+	// and checksums the packet. The return value is the checksummeed value.
+	boost::uint16_t checksum(boost::uint8_t* packet, int length, int index);
 }
