0076e4d788399c5ced0de812e5c852db96ad64c9
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-05-27 02:30:17 +0000

    Added a mega-comment. Deal with it.

--- gogo/src/MUIDSanta.cpp
+++ gogo/src/MUIDSanta.cpp
@@ -15,5 +15,11 @@ MUID MUIDSanta::get()
 
 void MUIDSanta::give_back(MUID /*id*/)
 {
-	// Unimplemented for now... we don't need it. Maybe next week.
+	// Unimplemented for now... we don't need it until the number of clients
+	// exceeds 2^64. In order for that to happen, the entire population of
+	// the world would need to connect 2.75 * 10^9 times. And even then, this
+	// algorithm won't generate any collisions unless there's been someone
+	// on from the very beginning. Suffice to say, there's no pressing need
+	// to make this algorithm "right", or any more complex until a bug report
+	// is filed.
 }
