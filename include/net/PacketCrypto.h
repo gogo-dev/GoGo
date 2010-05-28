ec4918bff46d478f5977c219b9a1f3c955860ada
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-05-28 21:27:46 +0000

    Moved packet crypto into the net library.

--- include/net/PacketCrypto.h
+++ include/net/PacketCrypto.h
@@ -7,13 +7,13 @@ namespace packet
 	// and the encryption key. The return value is equal to "packet",
 	// and the buffer "packet" is decrypted.
 	boost::uint8_t* decrypt(boost::uint8_t* packet, int length, int index,
-			const boost::uint8_t* key);
+							const boost::uint8_t* key);
 
 	// Takes a packet, the packet length, the index to start encryption,
 	// and the encryption key. The return value is equal to "packet",
 	// and the buffer "packet" is decrypted.
 	boost::uint8_t* encrypt(boost::uint8_t* packet, int length, int index,
-			const boost::uint8_t* key);
+							const boost::uint8_t* key);
 
 	// Takes a packet, the packet length, the index to start checksumming
 	// and checksums the packet. The return value is the checksummeed value.
