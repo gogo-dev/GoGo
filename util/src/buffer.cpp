59f28eac5a3ed60b5bbfdc7db63903121bb27606
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-05-30 03:47:31 +0000

    Got an initial copy of the protocol parser done. It still needs to have conflicts resolve inside the .protocol files before it's usable, though.

--- util/src/buffer.cpp
+++ util/src/buffer.cpp
@@ -1,26 +1,42 @@
 #include <util/buffer.h>
 #include <util/memory.h>
 #include <boost/checked_delete.hpp>
+#include <cstddef>
 
 using namespace std;
 using namespace boost;
 
 Buffer::Buffer(size_t _length)
-	: data_(new uint8_t[_length])
 {
 	length_ = _length;
+
+	if(length_ > 0)
+		data_ = new uint8_t[_length];
+	else
+		data_ = NULL;
 }
 
 Buffer::Buffer(const void* initialData, size_t _length)
 {
 	length_ = _length;
+
+	if(length_ > 0)
+		data_ = new uint8_t[length_];
+	else
+		data_ = NULL;
+
 	memory::copy(data_, initialData, length_);
 }
 
 Buffer::Buffer(const Buffer& other)
-	: data_(new uint8_t[other.length_])
 {
 	length_ = other.length_;
+	
+	if(length_ > 0)
+		data_ = new uint8_t[length_];
+	else
+		data_ = NULL;
+
 	memory::copy(data_, other.data_, length_);
 }
 
