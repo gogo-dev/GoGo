3f852fbbe2bc1c8e479349a1417036840e27b8f1
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-06 03:57:19 +0000

    Got the initial framework for the cockpit module hashed out, and migrated the packet parameter processing over.

--- include/cockpit/Socket.h
+++ include/cockpit/Socket.h
@@ -1,27 +1,16 @@
 #pragma once
-#include <boost/asio/ip/tcp.hpp>
 
 namespace cockpit {
 
-namespace packet {
-	class Packet;
-	class Registry;
-}
+namespace packet { class Packet; }
 
 class Socket
 {
-private:
-	packet::Registry* registry;
-
 public:
-	Socket(boost::asio::io_service* io);
-
-	void set_registry(packet::Registry* registry);
+	Socket();
 
 	void begin();
 	void disconnect();
-
-	boost::asio::ip::tcp::socket& get_raw();
 
 	void send(const packet::Packet* packet);
 
