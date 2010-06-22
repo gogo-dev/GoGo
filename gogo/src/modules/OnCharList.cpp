bad2d0c37de652bacdde0467d35e6a8b84124ee5
Jacob <jacob@trinityent.org>
2010-06-22 01:54:32 +0000

    Clark Gaebel says (9:52 PM):  not looking right now  just building, then testing  fuuuu  CMake Error in gogo/src/CMakeLists.txt:   Cannot find source file "OnCharList.cpp". Clark Gaebel says (9:53 PM):  you broke the makefiles   Phail says (9:53 PM):  did not  WORKS FOR ME Clark Gaebel says (9:53 PM):  there is no OnCharList.cpp  fail @ commit   Phail says (9:53 PM):  oh  my bad  let me commit it Clark Gaebel says (9:53 PM):  yeah. it really is.

---
+++ gogo/src/modules/OnCharList.cpp
@@ -0,0 +1,36 @@
+#include "../GoGoClient.h"
+
+#include <database/oopsies.h>
+#include <cockpit/Transmitter.h>
+#include <cockpit/packet/protocol/Match_ResponseAccountCharList.h>
+#include <cockpit/packet/Parameters.h>
+
+#include <vector>
+using namespace std;
+using namespace boost;
+using namespace cockpit;
+using namespace packet;
+
+void GoGoClient::OnCharList()
+{
+	std::vector<CharacterEntry> charList = database->GetCharacterList(myAccount.AccountId);
+	blob charBlob (charList.size(), 34);
+	logger->info(format("Character Count: %1%") % charList.size());
+
+	if (charList.size() == 0)
+	{
+		for (int i = 0; i < 32; ++i)
+			charBlob.add_param(packet::uint8(0));
+	}
+	else
+	{
+		for (size_t i = 0; i < charList.size(); ++i)
+		{
+			charBlob.add_param(packet::blob_string(charList[i].CharacterName.c_str(), 32));
+			charBlob.add_param(packet::uint8(charList[i].CharacterIndex));
+			charBlob.add_param(packet::uint8(charList[i].CharacterLevel));
+		}
+	}
+
+	transmitter->send(&protocol::Match_ResponseAccountCharList(charBlob));
+}
