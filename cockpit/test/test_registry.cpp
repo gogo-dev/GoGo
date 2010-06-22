ea8b281c2b05096504e4a882e8a6a701d43d46ab
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-22 03:24:48 +0000

    Added a registry test to make sure all's good when there's no registered handler.

---
+++ cockpit/test/test_registry.cpp
@@ -0,0 +1,18 @@
+#include <test.h>
+#include <cockpit/packet/Registry.h>
+
+#include <cstddef>
+
+void test_no_handler_for_call()
+{
+	cockpit::packet::Registry r;
+
+	r.dispatch(531, NULL, 0);
+}
+
+int test_main(int, char**)
+{
+	test_no_handler_for_call();
+
+	return 0;
+}
