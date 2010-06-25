7a84767b4c283d5bf6599a4209dcd12ed39284ad
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-25 23:33:04 +0000

    ChannelList is done. Next up - channels.

--- gogo/src/ChannelList.cpp
+++ gogo/src/ChannelList.cpp
@@ -1,6 +1,26 @@
 #include "ChannelList.h"
 
-// TODO(Clark): ChanneList implementation.
+using namespace std;
+using namespace boost;
+
+vector<const Channel*> ChannelList::get_some(function<bool (const Channel*)> predicate) const
+{
+	vector<const Channel*> ret(activeChannels.size());
+
+	for(CList::const_iterator i = activeChannels.begin(), end = activeChannels.end(); i != end; ++i)
+	{
+		const Channel* current = &(*i);	// That's right!
+		if(predicate(current))
+			ret.push_back(current);
+	}
+
+	return ret;
+}
+
+const vector<Channel*>* ChannelList::get_all() const
+{
+	return &shortcut;
+}
 
 ChannelList::~ChannelList()
 {
--- gogo/src/ChannelList.h
+++ gogo/src/ChannelList.h
@@ -2,17 +2,25 @@
 #include "Channel.h"
 
 #include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+
+#include <algorithm>
+#include <vector>
 
 #include <util/SmallVector.h>
 
-// TODO(Clark): Flesh this out.
 class ChannelList : boost::noncopyable
 {
 private:
+	typedef SmallVector<Channel, 16> CList;
 	// By making this read-only, we remove our need for mutexing. The only
 	// problem with this, though, is that each channel must now hold their own
 	// mutex.
-	const SmallVector<Channel, 16> activeChannels;
+	CList activeChannels;
+
+	// Provides a nice easy way to access all the channels in activeChannels,
+	// without having to know how they're stored.
+	std::vector<Channel*> shortcut;
 
 public:
 	/**
@@ -25,9 +33,25 @@ public:
 	ChannelList(const IteratorType& begin, const IteratorType& end)
 		: activeChannels(begin, end)
 	{
+		shortcut.reserve(activeChannels.size());
+		for(CList::iterator i = activeChannels.begin(), end = activeChannels.end(); i != end; ++i)
+			shortcut.push_back(&(*i));
 	}
 
-	//
+	/**
+		Returns a vector of channels for every channel for which the predicate
+		holds.
+
+		@param  predicate When this returns true, the parameter will be one of
+		                  the returned channels.
+
+		@return A full list of every channel for which the predicate held true.
+	*/
+	std::vector<const Channel*> get_some(boost::function<bool (const Channel*)> predicate) const;
+
+	/// Returns pointers to every single channel. Please try to avoid using
+	/// this function in favor of get() as much as possible.
+	const std::vector<Channel*>* get_all() const;
 
 	~ChannelList();
 };
