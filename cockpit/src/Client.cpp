b85f5bf46bb161eab43620e3d42ae4f04e832567
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-27 00:23:04 +0000

    Wow that could've been fatal.

--- cockpit/src/Client.cpp
+++ cockpit/src/Client.cpp
@@ -251,7 +251,7 @@ void Client::on_payload(uint8_t* p, uint16_t payloadSize, bool encrypted, system
 	uint16_t paramLength = payloadSize - Client::PayloadHeader::SIZE;	// LOL.
 	uint8_t* params = p + Client::PayloadHeader::SIZE;
 
-	if(payload.packetID != sending_currentPacketID++)
+	if(payload.packetID != recieving_currentPacketID++)
 	{
 		logger->info(
 			format("[%1%] Invalid packetID (%2% instead of %3%) recieved. Disconnecting.")
