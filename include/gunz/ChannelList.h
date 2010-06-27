beafd9a191791491bf4898d0b6de96712d3112aa
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-27 00:21:41 +0000

    Ooops, forgot the headers D:

---
+++ include/gunz/ChannelList.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <gunz/Channel.h>
+
+#include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+
+#include <algorithm>
+#include <vector>
+
+#include <util/SmallVector.h>
+
+class ChannelList : boost::noncopyable
+{
+private:
+	typedef SmallVector<Channel, 16> CList;
+	// By making this read-only, we remove our need for mutexing. The only
+	// problem with this, though, is that each channel must now hold their own
+	// mutex.
+	CList activeChannels;
+
+	// Provides a nice easy way to access all the channels in activeChannels,
+	// without having to know how they're stored.
+	std::vector<Channel*> shortcut;
+
+public:
+	/**
+		Constructs a channel list from a container of any type, by passing in
+		that container's iterators. NOTE: It MUST be a container of Channel. I
+		don't know why I have to explicitly mention that, but I like to think
+		it's good to know.
+	*/
+	template <typename IteratorType>
+	ChannelList(const IteratorType& begin, const IteratorType& end)
+		: activeChannels(begin, end)
+	{
+		shortcut.reserve(activeChannels.size());
+		for(CList::iterator i = activeChannels.begin(), end = activeChannels.end(); i != end; ++i)
+			shortcut.push_back(&(*i));
+	}
+
+	/**
+		Returns a vector of channels for every channel for which the predicate
+		holds.
+
+		@param  predicate When this returns true, the parameter will be one of
+		                  the returned channels.
+
+		@return A full list of every channel for which the predicate held true.
+	*/
+	std::vector<const Channel*> get_some(boost::function<bool (const Channel*)> predicate) const;
+
+	/// Returns pointers to every single channel. Please try to avoid using
+	/// this function in favor of get() as much as possible.
+	const std::vector<Channel*>* get_all() const;
+
+	~ChannelList();
+};
---
+++ include/gunz/PlayerTraits.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+#include <boost/cstdint.hpp>
+
+#include <gunz/simple_types.h>
+
+struct PlayerTraits
+{
+	enum PlayerLocation
+	{
+		PL_OUTSIDE,
+		PL_LOBBY,
+		PL_STAGE,
+		PL_BATTLE
+	};
+
+	MUID muid;
+	std::string name;
+	std::string clanName;
+	boost::uint8_t level;
+	PlayerLocation location;
+	boost::uint8_t ugradeid;
+	boost::int32_t clanid;
+
+	PlayerTraits();
+
+	PlayerTraits(
+		MUID muid,
+		const std::string& name,
+		const std::string& clanName,
+		boost::uint8_t level,
+		PlayerLocation location,
+		boost::uint8_t ugradeid,
+		boost::int32_t clanid
+	);
+
+	~PlayerTraits();
+};
