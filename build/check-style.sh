9961871a9ba4f66995d8544b84c617557557ab79
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-06-27 16:06:30 +0000

    Got rid fo the failure check-style and moved MUIDSanta.

--- build/check-style.sh
+++
@@ -1,1 +0,0 @@
-cppcheck --enable=all --force -I /usr/include/mysql -I /usr/include/mysql++ -I /home/clark/dev/cpp/GoGo/include  -I /usr/include -j3 .. 2> issues.txt
---
+++ include/gunz/MUIDSanta.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <gunz/simple_types.h>
+
+// This class controls the MUIDs of various in-game objects.
+class MUIDSanta
+{
+private:
+	volatile MUID next;	// The "next" MUID to give out.
+
+public:
+	MUIDSanta();
+	MUID get();
+	void give_back(MUID id);
+};
