995bebc9d0a2d6c76a6dc6b53ca82ff19df63ddd
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-07-02 19:35:45 -0400

    Re-added ChannelTraits. Now that I've had some time to think about it, yes, it was a good idea.

---
+++ gunz/src/ChannelTraits.cpp
@@ -0,0 +1,69 @@
+#include <gunz/ChannelTraits.h>
+
+using namespace std;
+using namespace boost;
+
+namespace gunz {
+
+ChannelTraits::ChannelTraits()
+	: uid(0),
+	  name("<<unnamed>>"),
+	  maxPlayers(0),
+	  rule(CR_ELITE),
+	  type(CT_GENERAL),
+	  minLevel(0),
+	  maxLevel(0)
+{
+}
+
+ChannelTraits::ChannelTraits(const ChannelTraits& other)
+	: uid(other.uid),
+	  name(other.name),
+	  maxPlayers(other.maxPlayers),
+	  rule(other.rule),
+	  type(other.type),
+	  minLevel(other.minLevel),
+	  maxLevel(other.maxLevel)
+{
+}
+
+ChannelTraits::ChannelTraits(
+	MUID _uid,
+	const std::string& _name,
+	boost::uint32_t _maxPlayers,
+	ChannelRule _rule,
+	ChannelType _type,
+	boost::uint8_t _minLevel,
+	boost::uint8_t _maxLevel)
+	  : uid(_uid),
+	    name(_name),
+	    maxPlayers(_maxPlayers),
+	    rule(_rule),
+	    type(_type),
+	    minLevel(_minLevel),
+	    maxLevel(_maxLevel)
+{
+}
+
+ChannelTraits& ChannelTraits::operator=(ChannelTraits other)
+{
+	std::swap(other, *this);
+	return *this;
+}
+
+// The following two functions depend on the uniquity of uids.
+bool ChannelTraits::operator<(const ChannelTraits& other) const
+{
+	return uid < other.uid;
+}
+
+bool ChannelTraits::operator==(const ChannelTraits& other) const
+{
+	return uid == other.uid;
+}
+
+ChannelTraits::~ChannelTraits()
+{
+}
+
+}
---
+++ include/gunz/ChannelTraits.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <gunz/simple_types.h>
+
+#include <string>
+
+#include <boost/cstdint.hpp>
+
+namespace gunz {
+
+struct ChannelTraits
+{
+	MUID uid;
+	std::string name;
+	boost::uint32_t maxPlayers;
+	ChannelRule rule;
+	ChannelType type;
+	boost::uint8_t minLevel;
+	boost::uint8_t maxLevel;
+
+	ChannelTraits();
+	ChannelTraits(const ChannelTraits& other);
+
+	// Doesn't do anything fancy, it only exists for convenience.
+	ChannelTraits(MUID uid,
+				  const std::string& name,
+				  boost::uint32_t maxPlayers,
+				  ChannelRule rule,
+				  ChannelType type,
+				  boost::uint8_t minLevel,
+				  boost::uint8_t maxLevel);
+
+	ChannelTraits& operator=(ChannelTraits other);
+
+	bool operator<(const ChannelTraits& other) const;
+	bool operator==(const ChannelTraits& other) const;
+
+	~ChannelTraits();
+};
+
+}
