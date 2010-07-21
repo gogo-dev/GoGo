d7fedd7012cbfdd00c46f5cfa6697dce1793bc83
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-07-20 22:22:09 -0400

    Added the threading portions of the code.

--- include/util/WorkQueue.h
+++ include/util/WorkQueue.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <cassert>
 #include <cstddef>
+#include <functional>
 
+#include <boost/bind/bind.hpp>
 #include <boost/call_traits.hpp>
+#include <boost/ref.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/mutex.hpp>
 
 /**
 	This class aims to be the perfect queue for a single thread producing and a
 	single, different thread consuming. Elements will be consumed in the order
-	they are produced.
+	they are produced. There MUST be only one producer and one consumer thread.
+	If there is not, undefined behavior results.
 
 	INTERNALS:
 
@@ -15,42 +21,80 @@
 		buffer. This offers good locality of reference, as well as good
 		amortized efficiency. A minor drawback of this approach is iterator
 		invalidity, but that shouldn't be a problem given the sparse interface.
-
-		//
 */
 template <typename ElemTy>
 class WorkQueue
 {
+	typedef typename boost::call_traits<ElemTy>::param_type ParamType;
 private:
-	size_t minSize;
+	size_t minSize;  // The size of the container will NEVER drop below
+	                 // this amount.
 
-	size_t currentCapacity;
-	size_t numElems;
+	size_t capacity; // This number is actually log2(realCapacity). This
+	                 // allows us to replace a modulus with a shift.
 
-	ElemTy* buffer;
-	ElemTy* head;
-	ElemTy* tail;
+	size_t numElems; // Duh.
+
+	ElemTy* buffer;  // The pointer to the beginning of the internal
+	                 // buffer.
+
+	ElemTy* head;    // The pointer to the head of the circular buffer.
+	ElemTy* tail;    // The pointer to the tail of the circular buffer.
+
+	boost::mutex protection;
+	boost::condition_variable cond;
 
 private:
-	void resize(size_t newSize)
+
+	// Increments a pointer, compensating for the circular nature of the buffer.
+	ElemTy* increment_internal_pointer(ElemTy* ptr)
 	{
-		assert(newSize > currentCapacity);
+		// This will ONLY work if capacity is a power of two.
+		return ((ptr - buffer + 1) & (capacity - 1)) + buffer;
+	}
 
-		ElemTy* newBuffer = new ElemTy[newSize];
+	void resize(size_t newCap)
+	{
+		ElemTy* newBuffer = new ElemTy[newCap];
 
 		for(size_t i = 0; i < numElems; ++i)
-			newBuffer[i] = *head++;
+		{
+			newBuffer[i] = *head;
+			head = increment_internal_pointer(head);
+		}
 
 		delete[] buffer;
 		buffer = newBuffer;
 
 		head = newBuffer;
 		tail = newBuffer + numElems;
+
+		capacity = newCap;
 	}
 
-	static bool buffer_has_wrapped(ElemTy* head, ElemTy* tail)
+	void grow()
 	{
-		return head < tail;
+		assert(
+			numElems == capacity
+		 && "We're trying to grow the buffer without it being necessary!"
+		);
+
+		resize(capacity * 2);
+	}
+
+	void shrink()
+	{
+		assert(
+			numElems < (capacity / 2)
+		 && "If we try to shrink the array now (we are), we will corrupt memory."
+		);
+
+		resize(capacity / 2);
+	}
+
+	inline bool queue_has_element()
+	{
+		return numElems != 0;
 	}
 
 public:
@@ -60,10 +104,15 @@ public:
 	*/
 	WorkQueue(size_t defaultSize = 5)
 	{
-		currentCapacity = 1 << defaultSize;
+		if(defaultSize < 2)
+			defaultSize = 2;
+
+		capacity = 1 << defaultSize;
+		minSize =  capacity;
+
 		numElems = 0;
 
-		buffer = new ElemTy[currentCapacity];
+		buffer = new ElemTy[capacity];
 		head = buffer;
 		tail = buffer;
 	}
@@ -74,12 +123,23 @@ public:
 		copied - similar in behavior to a vector. For efficiency reasons, the
 		size will always be doubled when a resize must happen.
 	*/
-	void push(typename boost::call_traits<ElemTy>::param_type elem)
+	void push(ParamType elem)
 	{
-		if((numElems + 1) > currentCapacity)
-			resize(2 * currentCapacity);
+		{
+			boost::unique_lock<boost::mutex> w(protection);
 
-		static_cast<void>(elem);
+			size_t newSize = numElems + 1;
+
+			if(newSize > capacity)
+				grow();
+
+			*tail = elem;
+			tail = increment_internal_pointer(tail);
+
+			numElems = newSize;
+		}
+
+		cond.notify_one();
 	}
 
 	/**
@@ -93,7 +153,25 @@ public:
 	*/
 	ElemTy pop()
 	{
-		return ElemTy();
+		using namespace std;
+		using namespace boost;
+
+		unique_lock<mutex> w(protection);
+
+		cond.wait(w, bind(not_equal_to<size_t>(), cref(numElems), 0));
+
+		size_t newSize = numElems - 1;
+
+		if(newSize < capacity / 4 && newSize >= (minSize * 2))
+			shrink();
+
+		ElemTy& ret = *head;
+
+		head = increment_internal_pointer(head);
+
+		numElems = newSize;
+
+		return ret;
 	}
 
 	~WorkQueue()
--- util/test/test_workqueue.cpp
+++ util/test/test_workqueue.cpp
@@ -2,9 +2,19 @@
 
 #include <util/WorkQueue.h>
 
-TEST(workqueue, no_edge_conditions)
+#include <boost/bind/bind.hpp>
+#include <boost/thread.hpp>
+
+using namespace boost;
+
+TEST(work_queue, construction)
 {
 	WorkQueue<int> q;
+}
+
+TEST(work_queue, no_edge_conditions)
+{
+	WorkQueue<int> q(2);
 
 	q.push(3);
 	q.push(2);
@@ -15,13 +25,57 @@ TEST(workqueue, no_edge_conditions)
 	EXPECT_EQ(1, q.pop());
 }
 
-TEST(workqueue, still_works_after_resize)
+// Elements are continuously added and removed, wrapping aruond the structure
+// like a snake!
+TEST(work_queue, snake)
+{
+	WorkQueue<int> q(2);
+
+	for(int i = 0; i < 1000; ++i)
+	{
+		q.push(i);
+		q.push(i);
+		EXPECT_EQ(i, q.pop());
+		EXPECT_EQ(i, q.pop());
+	}
+}
+
+TEST(work_queue, still_works_after_resize)
+{
+	WorkQueue<int> q(2);
+
+	// Do it three times to test growing AND shrinking.
+	for(int z = 0; z < 3; ++z)
+	{
+		for(int i = 0; i < 1000; ++i)
+			q.push(i);
+
+		for(int i = 0; i < 1000; ++i)
+			EXPECT_EQ(i, q.pop());
+	}
+}
+
+#define ELEMENTS_IN_QUEUE	8
+
+static void producer(WorkQueue<int>& q)
+{
+	for(int i = 0; i < ELEMENTS_IN_QUEUE; ++i)
+		q.push(i);
+}
+
+static void consumer(WorkQueue<int>& q)
+{
+	for(int i = 0; i < ELEMENTS_IN_QUEUE; ++i)
+		EXPECT_EQ(i, q.pop());
+}
+
+TEST(work_queue, concurrent_accesses)
 {
 	WorkQueue<int> q;
 
-	for(int i = 0; i < 1000; ++i)
-		q.push(i);
+	thread p(bind(producer, ref(q)));
+	thread c(bind(consumer, ref(q)));
 
-	for(int i = 0; i < 1000; ++i)
-		EXPECT_EQ(i, q.pop());
+	p.join();
+	c.join();
 }
